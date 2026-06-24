89c90612f94a9a80f45b5c94a8fc694e79ba036b
Elena Korovina-Giordano <elena.giordano@yandex.ru>
2026-06-25 00:16:51 +0300

    Create 01_mau_authors.sql

---
+++ sql/01_mau_authors.sql
@@ -0,0 +1,13 @@
+SELECT
+    a.main_author_name,
+    COUNT(DISTINCT au.puid) AS mau
+FROM bookmate.audition AS au
+JOIN bookmate.content AS c
+    ON au.main_content_id = c.main_content_id
+JOIN bookmate.author AS a
+    ON c.main_author_id = a.main_author_id
+WHERE au.msk_business_dt_str >= '2024-11-01'
+  AND au.msk_business_dt_str < '2024-12-01'
+GROUP BY a.main_author_name
+ORDER BY mau DESC
+LIMIT 3;
